b0c886b98550bf322bef58a4fd46bca73c0780bd
Scott Lindsey <scott.lindsey@mheducation.com>
2017-05-19 10:52:50 -0700

    tidied keys and fixed wrong chmod

--- ssh.pl
+++ ssh.pl
@@ -22,7 +22,7 @@ if (!-e $authorized_keys){
     print "Created $authorized_keys (0600)\n";
 }
 `chmod g-w $home`;
-`chmod a-w $home`;
+`chmod o-w $home`;
 
 # ------------------------------------------------------------------------------
 
@@ -53,7 +53,7 @@ foreach (@lines){
     print AUTHKEY "$_\n";
 }
 foreach (@$keys){
-    print AUTHKEY "$_ $key_identifier\n";
+    print AUTHKEY "$_-$key_identifier\n";
 }
 close AUTHKEY;
 
@@ -62,8 +62,12 @@ print "inserted public keys in $authorized_keys\n";
 
 sub keys{
     return [
-        'ssh-dss AAAAB3NzaC1kc3MAAAEBALRT0ogrhTa5pEmQR+lDPyHQy1cWTQhef68snQOkSApQWxDQ7CCopKftKgvE4rVAMmsQ5k+Fv1TWLKXziC//Kkgd7cjMyAJn0/JHWmM62/4+ReDqdApeSP2GvjjoDp1kUneu1M/z8aOa+Z3PpZkYwpAMb3FGKpAZ4epGVbjdm89EOY+NFqNkroPKI4h/n7WB9TchJgSD376d8dhh1aGHU13p2G3fO6FQERiTGBhTHcA4ReU1dvjEJXn6RgpS3KG3ZMYDdM7ZX+ctNn8hU0GONPFUTio25rqKgeHn9uIvOx/JwVQlpp2BKA7ZVIVZseSU1e7Q+H8yzHc7uVjits2OZcEAAAAVAPDRFhQaxg0484mrp39ua9tT+GHvAAABAB0H0gZoF7v6WW7JARoREI6y3YIhWMHm5q7py77NqGi+FaQRTMgVx1fnYxJzaC17HqINSuAfqqY2BESX5LG4PtN+R09XEam6efk8bt6i9j+CGKgu0zX4gp5rldOuG0AUKrLQOkT7gSI9kUVrokkuSuDril4B2ETSzf154edX8bH87El//kYf/NUaYCdVDsYk0kWPJbY9+VB916T4Qo5Ycim9/1nr4cMqtV1bV3dQnxWISLaoUB/8VY/mqRLlZZ/1RnKJsGR6So7TMkmLxl3T9eQoZlkuIeFzKnQ4tpXJe/+GoedN87jxl1VR2wt81WxvgsLMxoxiL+uXdoimASp6vXQAAAEARzxcKTtNZmkazTxjL56IA2umA5CF49725QDFejPbXXlaVtgCa6rNFsaFHI6n4UkVeYHmwd92vYJtiKPaFtYNgSRxfAQQnhorwgi6FQgc3e+DuuGRiqaNocD4frYDwz9uWsCWjMfwrylQrDVpIL1ZtP4d1c7Ow9J2C+VRy6Ndplbx82t4mR4ekZmbbM34WVYnrXatnclRr225yspVZmmj8NPzPVvFcwFC2ehsJLFMJnVqf4kOq9un0a0qSoIJjzg+qqNKX47ZWcup5RnutmfRTn8U3U8CfDnycYX5hoH+5/h0SXatsVS0wA2+zazOPWAhrfavwC2zJm2cshI20qLAlg== scottl@scott.to',
-        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVXG5NmmBvhKxA1t2H/qInOSWjYLNNaBRLQaI5rPbKX5U9pQfKJWHVnH779xbCFywkPed8wveIqfKfuOdNCHm59XrCeHVq1Y/2y6WsWQCyVR19xtRkLDhYuOuVC0bXsjloOYtlsXR4+r1x9lfHcV/6a74OzVuIgf79FDuXPNVi6Y9TOfpzhGRfNCzSE/4M0mMA364OuxSXGJTMLaQGlCioPzWSmqotITvwVjHzW3wEK4mmTpdIodjwVTsNjH86Gt7uE9x0tXgNSgVhmLmZ1v6DVRESm5EdLWce+hz766nSVYMteqpH5dS2eWN3tWOAIZu8AaTPlccfKQvNZIxZ2Siv slindsey@laptop'
+        # lappy
+        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDVXG5NmmBvhKxA1t2H/qInOSWjYLNNaBRLQaI5rPbKX5U9pQfKJWHVnH779xbCFywkPed8wveIqfKfuOdNCHm59XrCeHVq1Y/2y6WsWQCyVR19xtRkLDhYuOuVC0bXsjloOYtlsXR4+r1x9lfHcV/6a74OzVuIgf79FDuXPNVi6Y9TOfpzhGRfNCzSE/4M0mMA364OuxSXGJTMLaQGlCioPzWSmqotITvwVjHzW3wEK4mmTpdIodjwVTsNjH86Gt7uE9x0tXgNSgVhmLmZ1v6DVRESm5EdLWce+hz766nSVYMteqpH5dS2eWN3tWOAIZu8AaTPlccfKQvNZIxZ2Siv slindsey@serval',
+        # mac
+        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrOhFLqPYYPIFUo9ebpRIX6QiYFWy9nPgNXLsKWMa/x09WyyGHW2qvTGEGupwx+Koc6y9yH5Wg6thQrOq5hZ+QGh5AV51ttE9WFLjyQ87v2O1LbitH5yw1kIRe3Wp5nERWURC10Wx9uameU9n/1TA44uM8xm+n5tWHDO4xZMxZzFFEG1dJccgBM1c31ZGGpqN2+WfYD8/pPkt40GkiVopRKcgtTHsOtjfJEtVtudl3TBAN7e/bAkUFe5DoN6yHOcm4DHot4prxuX/mMBbo4jrxcdFedgPThTbyr4NakXeDdtKWNx/NGeh9pUw00n0c/OwsU1m5HF+abLO9r8bXnZu9 scott_lindsey@macbook',
+        # tower
+        'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDcAG6yeW+Y5qceRuHhegrGw5W0CMi4/d/6Mjiobf27sqyR7dFXavx8QFRV6uJBiFJvnQicVi8gJOzWiWHQhIWho/AHba4sJTok2PnoLncwUtymc6koFD5gtTZKf2l+AxlDmx59wGsCB5EoIOGMa3R6JCPqHwm0KLkBWAkKcXQaR9fuo8uzOlWBN6w7K+j3ZBGwJ2gqaZnULwdZnaf0OO6liqvsQcQ9+IyukUgoV44rSklu1fuDxuA9On0jyIuGtcBU0nJPkrbzXgquSigQHcCBgXiscyNBoPr8xqRG4UpCtTBR88Jo6egosCPOR8jMAOgfEEahu6p90Ss1/1TnLf8sJbQXIQdoy60NeAK0pPaG1HlZsMgvtevI6YBzNT2karm7G6o4P9FQU1+5LVefeAZrMCIZTsuiT2DmuVDSh9vZCUthkwLXA2yhRau59B01P+8Jsknl1QIY0v/dLvQhP15jw8o1KPgIZ//pu2x1pZ3uJKaEl/HL6z2sNE79XDR90ndl+TOmpS/sthAPvTIWQxvcLsrMjFTNpTZLxu2ue1/BL6ZuB+7Rkcm5X9i1uzpSYuvWcj2zTLycztAVxWIWYjvhPfiiwmLjAqsVOHC6feZGvOIyuCD8j+4hs/08BTcDdLfCt/8UBdqRuwb7TGzymyFPJxue+fmxs6JyyZDZNPSPcQ== scott@toadie',
     ];
 }
 
